f5907ad4dac5a9da8f88e5bad4c69719f38c4e5f
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2025-06-17 13:14:15 +0530

    change render url

--- src/api/axiosConfig.jsx
+++ src/api/axiosConfig.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-
+// Create an axios instance with baseURL
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',  
+  // baseURL: 'http://localhost:4000/api/v1',  // Your backend base URL  
+  baseURL: 'https://blogserver-9ya4.onrender.com/api/v1',  // Your backend base URL
   
 });
 
